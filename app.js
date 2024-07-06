19c9fba134dec90f61506419ea49aa59e34762db
Kirby Robinson <kirby@Kirbys-MacBook-Pro.local>
2024-07-06 07:54:55 -0400

    async patterns explored

--- app.js
+++ app.js
@@ -1,12 +1,15 @@
-const { readFile} = require('fs')
+const { readFile, writeFile } = require('fs').promises
 
-const getText = (path) =>
-    return new Promise
-
-readFile('./contrent/first.txt', 'utf8', (err, data) {
-    if (err) {
-        return
-    } else {
-        console.log(data);
+const start = async() => {
+    try {
+        const first = await readFile('./content/first.txt', 'utf8');
+        const second = await readFile('./content/second.txt', 'utf8');
+        await writeFile('./content/result-mind-greade.txt', 
+            `THis is awwesome : ${first}, ${second}`);
+            {flag: 'a' }
+    } catch (error) {
+        console.log(error)
     }
-})
+}
+
+start()
